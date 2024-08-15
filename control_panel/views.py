beaf666287c862e5c56ef0d4aff5b120501f4ed5
whatqt <trutnev.07@list.ru>
2024-08-16 03:11:17 +0700

    new file:   control_panel/templates/control_panel.html 	new file:   control_panel/urls.py 	modified:   control_panel/views.py 	modified:   register/templates/index.html 	modified:   register/templates/login.html 	deleted:    register/templates/login_base.html 	modified:   register/templates/register.html 	modified:   warehouse_management/settings.py 	modified:   warehouse_management/urls.py

--- control_panel/views.py
+++ control_panel/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+def control_panel(requset: HttpRequest):
+    return render(requset, 'control_panel.html')
--- warehouse_management/urls.py
+++ warehouse_management/urls.py
@@ -7,4 +7,5 @@ from register.views import index
 urlpatterns = [
     path('', index),
     path('register/', include('register.urls')),
+    path('control_panel/', include('control_panel.urls'))
 ]
