47b0223e6fdb820133f8a5d9c8df785568f71397
whatqt <trutnev.07@list.ru>
2024-08-16 02:52:58 +0700

    first commit

---
+++ register/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request: HttpRequest):
+    return render(request, 'index.html')
+
+def register(request: HttpRequest):
+    if request.method == "POST":
+        username = request.POST.get('username').replace(' ', '')
+        password = request.POST.get('password').replace(' ', '')
+        check_replay_name = User.objects.filter(username=username)
+        if check_replay_name:
+            return HttpResponse('<h1>Такой ник занят</h1>')
+        User.objects.create_user(username=username, password=password)
+        auth = authenticate(request, username=username, password=password)
+        print(auth)
+        login(request, auth)
+        return HttpResponse('<h1>Вы зарегистрировались</h1>')
+    return render(request, 'register.html')
+
+@login_required    
+def log_out(requset: HttpRequest):
+    logout(requset)
+    return redirect('http://127.0.0.1:8000/register/login')
+
+def log_in(request: HttpRequest):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        auth = authenticate(request, username=username, password=password)
+        print(auth)
+        if auth:
+            login(request, auth)
+            return redirect('http://127.0.0.1:8000/')
+        else:
+            return HttpResponse('<h1>Неправильный логин или пароль</h1>')
+    return render(request, 'login.html')
+ 
---
+++ warehouse_management/settings.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-=l409*l&-sw^^@bqi=d-%5%m==v@w%3egzi=tu%4txp_riwfht'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'register'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'warehouse_management.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'warehouse_management.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'warehouse_management',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('PASSWORD_POSTGRES'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+LOGIN_URL = 'http://127.0.0.1:8000/register/login'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
